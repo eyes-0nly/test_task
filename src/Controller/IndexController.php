2f4457ae5ff465aa44cc8c459f17dc0a3eb79647
eyes-0nly <mailz.eyes.only@gmail.com>
2024-01-09 10:00:22 +0300

    Added controller with redirect to form

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_index', methods: ['GET'])]
+    public function index(): Response
+    {
+        return $this->redirectToRoute('app_contact', [], 308);
+    }
+}
